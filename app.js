5e9c7f52d36fc42f44ad53e416876ca62526c132
karnick singh <karnicksingh61@gmail.com>
2026-04-21 13:00:05 +0530

    cookies fix

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ const sessionOption = session({
   resave: false,
   saveUninitialized: true,
   cookie :{
-    expire: Date.now()+7*24*60*60*1000,
-    maxAge:7*24*60*60*1000,
+    expires: new Date(Date.now() + 7*24*60*60*1000), 
+    maxAge: 7*24*60*60*1000,
     httpOnly:true
   }
 });
